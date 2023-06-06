ae780c24c787c066bb8fab7210fe2aa6c5117f38
noldowalker <noldowalker@gmail.com>
2023-06-06 09:34:52 +0300

    Create README.md

--- Models/Dweller.cs
+++ Models/Dweller.cs
@@ -9,9 +9,6 @@ namespace OldTechApp.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        
-        //ToDo: колонка должна быть или с одинаковым названием, или как-то еще это все обдумать, как показывать платформе 
-        //ToDo: что именно эта колонка такая, может ее в бейз модел включить, но тогда выпадут системные сущности.
         public string Version { get; private set; }
     }
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# EF6OptimisticConcurrencyTest
+
+Simple console example of using https://www.npgsql.org/ef6/index.html in EF6. 
+The connection string is set in OldTechApp\App.config:9
+
